a6a0e12806a86ed2b47fec975cc1700855b11f94
Hui Zhou <hzhou321@anl.gov>
2018-11-07 13:03:18 -0600

    hydra-config: Adopting aclocal_subdir.m4
    These are necessary changes to make hydra configure system use the same
    subdir configure as the main mpich configure.
    
    The changes now also allows optionally skipping duplicate configure and
    build of `mpl` and `hwloc` by reusing the libtools convenience library
    build in the parent tree.

--- src/pm/hydra/tools/topo/hwloc/Makefile.mk
+++ src/pm/hydra/tools/topo/hwloc/Makefile.mk
@@ -8,12 +8,3 @@ libhydra_la_SOURCES += tools/topo/hwloc/topo_hwloc.c
 
 noinst_HEADERS += tools/topo/hwloc/topo_hwloc.h
 
-if hydra_use_embedded_hwloc
-AM_CPPFLAGS += @HWLOC_EMBEDDED_CPPFLAGS@
-AM_CFLAGS += @HWLOC_EMBEDDED_CFLAGS@
-
-# Append hwloc to the external subdirs, so it gets built first
-external_subdirs += tools/topo/hwloc/hwloc
-external_dist_subdirs += tools/topo/hwloc/hwloc
-external_libs += @HWLOC_EMBEDDED_LDADD@ @HWLOC_EMBEDDED_LIBS@
-endif
